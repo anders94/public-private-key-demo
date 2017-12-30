2c18413f0cabd66d92ec1397f3ee60c924814bc9
Anders Brownworth <anders-git@evantide.com>
2017-12-30 15:34:56 -0500

    initial import

---
+++ README.md
@@ -0,0 +1,32 @@
+# Publick Key / Private Key Demo
+A web-based demonstration of public / private key pair concepts.
+
+This is a very basic visual introduction to the concepts behind a public / private key pair.
+
+## Setup
+Get the code:
+
+```
+git clone https://github.com/anders94/public-key-private-key-demo.git
+```
+
+Install dependencies:
+
+```
+cd public-key-private-key-demo
+npm install
+```
+Run the server:
+
+```
+./bin/www
+```
+
+Point a web browser at the demo:
+
+```
+http://localhost:3000
+```
+
+## Send Thanks
+Bitcoin greatfully accepted: `1K3NvcuZzVTueHW1qhkG2Cm3viRkh2EXJp`
---
+++ public/javascripts/blockchain.js
@@ -0,0 +1,67 @@
+var difficulty = 4;        // number of zeros required at front of hash
+var maximumNonce = 500000; // limit the nonce to this so we don't mine too long
+
+// NOTE: Because there are 16 possible characters in a hex value, each time you incrament
+// the difficulty by one you make the puzzle 16 times harder. In my testing, a difficulty
+// of 6 requires a maximumNonce well over 500,000,000.
+
+/////////////////////////
+// global variable setup
+/////////////////////////
+var pattern = '';
+for (var x=0; x<difficulty; x++) {
+  pattern += '0';
+}
+
+/////////////////////////
+// functions
+/////////////////////////
+function sha256(block, chain) {
+  // calculate a SHA256 hash of the contents of the block
+  return CryptoJS.SHA256(getText(block, chain));
+}
+
+function updateState(block, chain) {
+  // set the card background red or green for this block
+  if ($('#block'+block+'chain'+chain+'hash').val().substr(0, difficulty) === pattern) {
+    $('#block'+block+'chain'+chain+'card').removeClass('card-error').addClass('card-success');
+  }
+  else {
+    $('#block'+block+'chain'+chain+'card').removeClass('card-success').addClass('card-error');
+  }
+}
+
+function updateHash(block, chain) {
+  // update the SHA256 hash value for this block
+  $('#block'+block+'chain'+chain+'hash').val(sha256(block, chain));
+  updateState(block, chain);
+}
+
+function updateChain(block, chain, txCount) {
+  // update all blocks walking the chain from this block to the end
+  for (var x = block; x <= 5; x++) {
+    if (x > 1) {
+      $('#block'+x+'chain'+chain+'previous').val($('#block'+(x-1).toString()+'chain'+chain+'hash').val());
+    }
+    updateHash(x, chain);
+    if (txCount)
+      for (var y=0; y<txCount; y++)
+        verifySignature(block, chain, y);
+  }
+}
+
+function mine(block, chain, isChain) {
+  for (var x = 0; x <= maximumNonce; x++) {
+    $('#block'+block+'chain'+chain+'nonce').val(x);
+    $('#block'+block+'chain'+chain+'hash').val(sha256(block, chain));
+    if ($('#block'+block+'chain'+chain+'hash').val().substr(0, difficulty) === pattern) {
+      if (isChain) {
+        updateChain(block, chain);
+      }
+      else {
+        updateState(block, chain);
+      }
+      break;
+    }
+  }
+}
