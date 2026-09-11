f3a18032c4893c4fc2e8f0fffb987e9d6111b39a
anupamme <mediratta@gmail.com>
2026-09-11 03:18:31 +0000

    fix: V-001 security vulnerability
    Automated security fix generated by OrbisAI Security

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,22 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/:page', function(req, res, next) {
-    res.render(req.params.page, {page: req.params.page});
+    switch (req.params.page) {
+        case 'blockchain':
+            return res.render('blockchain', {page: 'blockchain'});
+        case 'encryption':
+            return res.render('encryption', {page: 'encryption'});
+        case 'index':
+            return res.render('index', {page: 'index'});
+        case 'keys':
+            return res.render('keys', {page: 'keys'});
+        case 'signatures':
+            return res.render('signatures', {page: 'signatures'});
+        case 'transaction':
+            return res.render('transaction', {page: 'transaction'});
+        default:
+            return next();
+    }
 });
 
 module.exports = router;
